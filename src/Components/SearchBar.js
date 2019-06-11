4cc5604782f06a80919ccd4f9072fab14341071e
João Victor Oliveira <joao.oliveira@pitang.com>
2019-06-11 10:17:53 -0300

    correção SearchBar de função

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
 
+//atenção na ordem dos parâmetros...primeiro é event, aquele de valor default é o último (caso não passado fica undefined)
+export const _pesquisar = async (event, setRepos, usuario, request = fetch) => {
+    event.preventDefault();
+    let url = `https://api.github.com/users/${usuario}/repos`;
+    const response = await request(url);
+    const json = await response.json();
+    console.log(response);
+    if (response.status === 200) {
+        setRepos(json);
+    } else {
+        setRepos([]);
+    }
+}
+
+
 //componente funcional
 export default (props) => {
     const [usuario, setUsuario] = useState();
-
-    const pesquisar = async (event, request = fetch) => {
-        event.preventDefault();
-        let url = `https://api.github.com/users/${usuario.usuario}/repos`;
-        const response = await request(url);
-        const json = await response.json();
-        console.log(response);
-        if (response.status === 200) {
-            props.setRepos(json);
-        } else {
-            props.setRepos([]);
-        }
+    const pesquisar = (event) => {
+        _pesquisar(event, props.setRepos, usuario)
     }
-
     return (
         <div className="search">
             <form onSubmit={pesquisar} >
                 <input data-testid="input" type="text" placeholder="nome do usuário no github" onChange={(event) => {
                     let usuario = event.target.value;
-                    setUsuario({usuario});
+                    setUsuario(usuario);
                 }} />
                 <input data-testid="searchButton" type="submit" value="pesquisar" onClick={pesquisar} />
             </form>
