ee406a4978dbf11b494fb393c3ad1194ea975e55
João Victor Oliveira <joao.oliveira@pitang.com>
2019-06-27 09:10:17 -0300

    add formik

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,18 +1,18 @@
 import React, { useContext, useState } from 'react';
 import './SearchBar.css';
 import { loadingContext } from '../App';
-import { async } from 'q';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 //atenção na ordem dos parâmetros...primeiro é event, aquele de valor default é o último (caso não passado fica undefined)
-export const _pesquisar = async (event, showLoading, hideLoading, nomeUsuario, setUsuario, setRepos, request = fetch) => {
-    event.preventDefault()
+export const _pesquisar = async (showLoading, hideLoading, nomeUsuario, setUsuario, setRepos, request = fetch) => {
     showLoading('Carregando...')
 
     const urlRepos = `https://api.github.com/users/${nomeUsuario}/repos`;
     const urlUsuario = `https://api.github.com/users/${nomeUsuario}`;
 
     await Promise.all([request(urlUsuario), request(urlRepos)]).then((response) => {
-        Promise.all([response[0].json(), response[1].json()]).then((values) => {  
+        Promise.all([response[0].json(), response[1].json()]).then((values) => {
             if (!values[0].message) {
                 setUsuario(values[0], 'OK')
             } else {
@@ -30,27 +30,33 @@ export const _pesquisar = async (event, showLoading, hideLoading, nomeUsuario, s
     }
     )
 }
+const validation = yup.object().shape({
+    usuario: yup.string().required("Nenhum nome informado")
+})
 
 //componente funcional
 export default () => {
 
     const { showLoading, hideLoading, setUsuario, setRepos } = useContext(loadingContext);
-    const [nomeUsuario, setNomeUsuario] = useState("");
+    const [nomeUsuario] = useState("");
 
-    const pesquisar = (event) => {
-        _pesquisar(event, showLoading, hideLoading, nomeUsuario, setUsuario, setRepos)
+    const pesquisar = (nomeUsuario) => {
+        _pesquisar(showLoading, hideLoading, nomeUsuario.usuario, setUsuario, setRepos)
     }
 
     return (
         <div className="search">
-            <form onSubmit={pesquisar} >
-                <input id="field" data-testid="input" type="text" placeholder="nome do usuário no github" onChange={
-                    (event) => {
-                        setNomeUsuario(event.target.value)
-                    }
-                } />
-                <input data-testid="searchButton" type="submit" value="pesquisar" />
-            </form>
+            <Formik initialValues={nomeUsuario} onSubmit={pesquisar} validationSchema={validation}>
+                <Form>
+                    <div>
+                        <Field name="usuario" id="field-input" data-testid="input" type="text" placeholder="nome do usuário no github" />
+                        <Field data-testid="searchButton" type="submit" value="pesquisar" />
+                    </div>
+                    <div>
+                        <ErrorMessage className="error" component="span" name="usuario"/>
+                    </div>
+                </Form>
+            </Formik>
         </div>
     );
 }
